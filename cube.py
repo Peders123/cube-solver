3dd009d99273a32b6c9b27f87e1d89c171e0488e
Rory Caston <rory@caston.co.uk>
2022-06-14 23:07:19 +0100

    updated cube and face

--- cube.py
+++ cube.py
@@ -11,10 +11,68 @@ class Cube:
     def __str__(self):
 
         string = ""
-        for i in range(5,-1,-1):
-            string += self.sides[i].__str__()
+        string += "     --- \n"
+        string += "    |%s %s|\n" % (self.sides[5].state[0], self.sides[5].state[1])
+        string += "    |%s %s|\n" % (self.sides[5].state[2], self.sides[5].state[3])
+        string += " --- --- --- --- \n"
+        string += "|%s %s|%s %s|%s %s|%s %s|\n" % (self.sides[1].state[0], self.sides[1].state[1], \
+            self.sides[2].state[0], self.sides[2].state[1], \
+            self.sides[3].state[0], self.sides[3].state[1], \
+            self.sides[4].state[0], self.sides[4].state[1])
+        string += "|%s %s|%s %s|%s %s|%s %s|\n" % (self.sides[1].state[2], self.sides[1].state[3], \
+            self.sides[2].state[2], self.sides[2].state[3], \
+            self.sides[3].state[2], self.sides[3].state[3], \
+            self.sides[4].state[2], self.sides[4].state[3])
+        string += " --- --- --- --- \n"
+        string += "    |%s %s|\n" % (self.sides[0].state[0], self.sides[0].state[1])
+        string += "    |%s %s|\n" % (self.sides[0].state[2], self.sides[0].state[3])
+        string += "     --- "
+        
 
         return string
 
+    
+    def up(self, prime=False):
 
-print(Cube())
+        if not prime:
+            self.sides[5].rotate()
+            self.sides[1].state[0], self.sides[2].state[0], self.sides[3].state[0], self.sides[4].state[0] = \
+                self.sides[2].state[0], self.sides[3].state[0], self.sides[4].state[0], self.sides[1].state[0]
+            self.sides[1].state[1], self.sides[2].state[1], self.sides[3].state[1], self.sides[4].state[1] = \
+                self.sides[2].state[1], self.sides[3].state[1], self.sides[4].state[1], self.sides[1].state[1]
+        else:
+            self.sides[5].rotate(True)
+            self.sides[1].state[0], self.sides[2].state[0], self.sides[3].state[0], self.sides[4].state[0] = \
+                self.sides[4].state[0], self.sides[1].state[0], self.sides[2].state[0], self.sides[3].state[0]
+            self.sides[1].state[1], self.sides[2].state[1], self.sides[3].state[1], self.sides[4].state[1] = \
+                self.sides[4].state[1], self.sides[1].state[1], self.sides[2].state[1], self.sides[3].state[1]
+
+
+    def down(self, prime=False):
+
+        if not prime:
+            self.sides[0].rotate()
+            self.sides[1].state[2], self.sides[2].state[2], self.sides[3].state[2], self.sides[4].state[2] = \
+                 self.sides[4].state[2], self.sides[1].state[2], self.sides[2].state[2], self.sides[3].state[2]
+            self.sides[1].state[3], self.sides[2].state[3], self.sides[3].state[3], self.sides[4].state[3] = \
+                 self.sides[4].state[3], self.sides[1].state[3], self.sides[2].state[3], self.sides[3].state[3]
+        else:
+            self.sides[0].rotate(True)
+            self.sides[1].state[2], self.sides[2].state[2], self.sides[3].state[2], self.sides[4].state[2] = \
+                self.sides[2].state[2], self.sides[3].state[2], self.sides[4].state[2], self.sides[1].state[2]
+            self.sides[1].state[3], self.sides[2].state[3], self.sides[3].state[3], self.sides[4].state[3] = \
+                self.sides[2].state[3], self.sides[3].state[3], self.sides[4].state[3], self.sides[1].state[3]
+
+
+    def right(self, prime=False):
+
+        pass
+
+
+
+c = Cube()
+print(c)
+c.down()
+print(c)
+c.down(True)
+print(c)
--- face.py
+++ face.py
@@ -13,59 +13,30 @@ class Face:
 
     def create_test(self):
 
-        self.state = [1,2,3,8,4,7,6,5]
+        self.state = [1,2,4,3]
 
 
     def __str__(self):
 
         string = ""
-        string += " ----- \n"
-        string += "|% s % s % s|\n" % (self.state[0], self.state[1], self.state[2])
-        string += "|% s % s % s|\n" % (self.state[3], self.type, self.state[4])
-        string += "|% s % s % s|\n" % (self.state[5], self.state[6], self.state[7])
-        string += " ----- \n"
+        string += " --- \n"
+        string += "|% s % s|\n" % (self.state[0], self.state[1])
+        string += "|% s % s|\n" % (self.state[2], self.state[3])
+        string += " --- \n"
 
         return string
 
 
     def create_face(self, type):
 
-        for _ in range(8):
+        for _ in range(4):
             self.state.append(Square(type))
 
+    def rotate(self, prime=False):
 
-    def cw_rotation(self):
-
-        state = self.state[:]
-
-        self.state[1] = state[0]
-        self.state[2] = state[1]
-        self.state[4] = state[2]
-        self.state[7] = state[4]
-        self.state[6] = state[7]
-        self.state[5] = state[6]
-        self.state[3] = state[5]
-        self.state[0] = state[3]
-
-
-    def acw_rotation(self):
-
-        state = self.state[:]
-
-        self.state[0] = state[1]
-        self.state[1] = state[2]
-        self.state[2] = state[4]
-        self.state[4] = state[7]
-        self.state[7] = state[6]
-        self.state[6] = state[5]
-        self.state[5] = state[3]
-        self.state[3] = state[0]
-
-
-s = Face('W')
-s.create_test()
-print(s)
-s.cw_rotation()
-print(s)
-s.acw_rotation()
-print(s)
+        if not prime:
+            self.state[0], self.state[1], self.state[2], self.state[3] = \
+                self.state[2], self.state[0], self.state[3], self.state[1]
+        else:
+            self.state[0], self.state[1], self.state[2], self.state[3] = \
+                self.state[1], self.state[3], self.state[0], self.state[2]
